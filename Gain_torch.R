4cf83dde7063257e895b39e6781a67659a1e6787
Fdvanleeuwen <94541170+Fdvanleeuwen@users.noreply.github.com>
2025-03-06 19:58:15 +0100

    Add the training loop and some plots

--- Gain_torch.R
+++ Gain_torch.R
@@ -3,7 +3,8 @@ library(torch)
 library(mice)
 library(MASS)
 
-df = MASS::mvrnorm(n = 5, mu = c(0,0), Sigma = matrix(c(1, 0.8, 0.8, 1), ncol = 2, nrow = 2))
+
+df = MASS::mvrnorm(n = 100, mu = c(0,0), Sigma = matrix(c(1, 0.8, 0.8, 1), ncol = 2, nrow = 2))
 
 # add some missing values
 df_miss = mice::ampute(df)
@@ -31,6 +32,22 @@ df_mask_tens = torch_tensor(df_mask)
 # this is the input for the generator
 gen_inp = torch_cat(list(df_combine_tens, df_mask_tens), dim = 2)
 
+# This function generates a "hint matrix," typically used in data imputation algorithms.
+# The matrix is binary, indicating whether a value is included in the hint.
+# Parameters:
+# - hint_rate: Probability of including a value in the hint.
+# - no: Number of rows in the generated matrix.
+# - dim: Number of columns in the generated matrix.
+hint_matrix <- function(hint_rate, no, dim) {
+  # Create a probability matrix of random numbers between 0 and 1.
+  prob_matrix <- matrix(runif(no * dim), no, dim)
+  # Generate a binary hint matrix based on the hint rate.
+  hint <- ifelse((prob_matrix < hint_rate), 1, 0)
+  return(hint)
+}
+
+hint_mat = torch_tensor(hint_matrix(hint_rate = 0.05, no = nrow(df_mask), dim = ncol(df_mask)))
+
 # Build the generator 
 gen <- nn_module(
   "Generator",
@@ -55,8 +72,7 @@ gen <- nn_module(
       nnf_relu() |>
       self$fc2() |>
       nnf_relu() |>
-      self$fc3() |>
-      nnf_sigmoid()
+      self$fc3()  # Sigmoid activation for stardardised data
   }
 )
 
@@ -130,58 +146,115 @@ G_loss <- function(mask, D_pred, G_input, G_pred, alpha){
 
 # Initialize the gen/discrim
 input_dim <- dim(gen_inp)[2]  
-hidden_dim <- 4  
-input_dim_d <- dim(gen_inp)[2]/2
+hidden_dim <- 10  
+input_dim_d <- input_dim
 
 generator <- gen(input_dim, hidden_dim)
 discriminator <- discrim(input_dim_d, hidden_dim)
 
 # We need to define the optimizer
-
 g_optimizer <- optim_adam(generator$parameters, lr = 0.001, betas = c(0.5, 0.999))
 d_optimizer <- optim_adam(discriminator$parameters, lr = 0.001, betas = c(0.5, 0.999))
   
-# DISCRIMINATOR STEP
-d_optimizer$zero_grad()
+n_iter = 1000
+D_loss_all = c()
+G_loss_all = c()
 
-# Generate fake data
-output_G <- generator(gen_inp)
+# training the model
 
-# Discriminator predictions on fake data
-fake_pred <- discriminator(output_G$detach())  # Detach to avoid backprop through generator
+for (i in 1:n_iter){
+  # DISCRIMINATOR STEP
+  d_optimizer$zero_grad()
+  
+  # Generate fake data
+  output_G <- generator(gen_inp)
+  
+  # add hint matrix
+  
+  D_input = torch_cat(list(output_G$detach(), hint_mat), dim = 2)
+  
+  # Discriminator predictions on fake data
+  fake_pred <- discriminator(D_input)  # Detach to avoid backprop through generator
+  
+  # Discriminator loss
+  d_loss_val <- D_loss(mask = df_mask_tens, D_pred = fake_pred)
+  
+  # Backpropagation and optimization for discriminator
+  d_loss_val$backward()
+  d_optimizer$step()
+  
+  # GENERATOR STEP
+  g_optimizer$zero_grad()
+  
+  # Generate fake data again (important!)
+  output_G <- generator(gen_inp)
+  
+  # Discriminator predictions on new fake data
+  D_input2 = torch_cat(list(output_G, hint_mat), dim = 2) # No detach here!
+  output_D <- discriminator(D_input2)  
+  
+  # Generator loss
+  g_loss_val <- G_loss(mask = df_mask_tens, D_pred = output_D, 
+                       G_input = df_combine_tens, G_pred = output_G, alpha = 1)
+  
+  # Back prop and optimization for generator
+  g_loss_val$backward()
+  g_optimizer$step()
+  
+  # Save losses
+  D_loss_all = c(D_loss_all, as.numeric(d_loss_val$item()))
+  G_loss_all = c(G_loss_all, as.numeric(g_loss_val$item()))
+}
 
-# Discriminator loss
-d_loss_val <- D_loss(mask = df_mask_tens, D_pred = fake_pred)
+out_df = data.frame(niter = 1:n_iter, D_loss = D_loss_all, G_loss = G_loss_all)
 
-# Backpropagation and optimization for discriminator
-d_loss_val$backward()
-d_optimizer$step()
+out_df %>% 
+  ggplot(aes(x = niter)) +
+  geom_line(aes(y = D_loss, color = "D loss")) +
+  geom_line(aes(y = G_loss, color = "G loss")) +
+  theme_minimal()
 
-# GENERATOR STEP
-g_optimizer$zero_grad()
+# RMSE
+sum(((1 - df_mask) * df - (1 - df_mask) * output_G)^2) / sum(1 - df_mask)
 
-# Generate fake data again (important!)
-output_G <- generator(gen_inp)
+# plot the missing vs real data
+real = df * (1 - df_mask)
+imputed = as_array((1 - df_mask) * output_G)
 
-# Discriminator predictions on new fake data
-output_D <- discriminator(output_G)  # No detach here!
+helper_missing = data.frame(df_mask) %>% 
+  mutate(miss_ind = case_when(X1 == 1 & X2 == 1 ~ "None",
+                              X1 == 1 & X2 == 0 ~ "X1",
+                              X1 == 0 & X2 == 1 ~ "X2",
+                              X1 == 0 & X2 == 0 ~ "Both"))
 
-# Generator loss
-g_loss_val <- G_loss(mask = df_mask_tens, D_pred = output_D, 
-                     G_input = df_combine_tens, G_pred = output_G, alpha = 1)
+df_plot = data.frame(real_x1 = df[,1],
+                     real_x2 =df[,2],
+                     imputed_x1 = as_array(output_G)[,1],
+                     imputed_x2 = as_array(output_G)[,2]) %>% 
+  mutate(miss_ind = helper_missing$miss_ind)
 
-# Backpropagation and optimization for generator
-g_loss_val$backward()
-g_optimizer$step()
+p1 = df_plot %>% 
+  ggplot(aes(x = real_x1, y = real_x2)) +
+  geom_point(aes(color = miss_ind)) +
+  theme_minimal() + 
+  labs(x = "X1", y = "X2", title = "Real and masked data") 
+
+p2 = df_plot %>% 
+  ggplot(aes(x = imputed_x1, y = imputed_x2)) +
+  geom_point(aes(color = miss_ind)) +  
+  theme_minimal() + 
+  labs(x = "X1", y = "X2", title = "Real and Imputed data")
+
+library(patchwork)
+
+p1 + p2 +plot_layout(guides = "collect") & theme(legend.position = 'bottom')
 
 # TO DO
-# wrapper for training
 # mini batch
-# add hint matrix for discriminator
 # Normalization
 # initialization
 # dropout
-# add real data to discriminator loss
+# add real data to discriminator loss?
 
 
 
